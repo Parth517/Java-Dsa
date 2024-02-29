3634c6dfbe5060f369fd9aa9d6dacca77868a391
Parth Deshpande <parthjun27@gmail.com>
2024-02-29 10:02:26 +0530

    Move Zeroes to the end

--- src/MoveZero.java
+++ src/MoveZero.java
@@ -1,7 +1,17 @@
 public class MoveZero {
     public static void revArr(int[] arr,int st,int end){
         int j=0;
-        
+        for(int i =0;i<arr.length;i++){
+            if(arr[i]!=0 && arr[j]==0){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+            if(arr[j]!=0){
+                j++;
+            }
+        }
+
     }
     public static void printArray(int[] arr){
         for( int i=0;i<arr.length;i++){
